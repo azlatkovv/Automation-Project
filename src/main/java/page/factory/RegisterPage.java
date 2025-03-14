2052ec367f4972643325574e45801f0fe5e9e9a4
lenovo <azlatkovv@gmail.com>
2025-03-15 00:19:36 +0200

    First test completed

--- src/main/java/page/factory/RegisterPage.java
+++ src/main/java/page/factory/RegisterPage.java
@@ -1,35 +1,52 @@
 package page.factory;
 
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
+
+import java.security.Key;
 import java.time.Duration;
 
-public class RegisterPage extends BasePage{
+public class RegisterPage {
     public static final String PAGE_URL = "http://training.skillo-bg.com:4300/users/register";
     private final WebDriver webDriver;
     @FindBy(xpath = "//*[@formcontrolname ='username']")
     protected WebElement usernameField;
     @FindBy(xpath = "/html/body/app-root/div[2]/app-register/div/div/form/div[1]/span")
     protected WebElement usernameErrorField;
+    @FindBy(xpath = "/html/body/app-root/div[2]/app-register/div/div/form/div[2]/span")
+    protected WebElement emailErrorField;
     @FindBy(xpath = "//*[@formcontrolname ='email']")
     private WebElement emailField;
     @FindBy(xpath = "//*[@formcontrolname ='birthDate']")
     private WebElement birthDayField;
     @FindBy(id = "defaultRegisterFormPassword")
     private WebElement passwordField;
-    @FindBy(xpath = "formcontrolname='confirmPassword']")
+    @FindBy(xpath = "//*[@formcontrolname='confirmPassword']")
     private WebElement confirmPasswordField;
-    @FindBy(xpath = "formcontrolname='publicInfo']")
+    @FindBy(xpath = "//*[@formcontrolname='publicInfo']")
     private WebElement publicInfoField;
     @FindBy(id ="sign-in-button")
     private WebElement signInButton;
+    @FindBy(xpath = "//*[@class = 'toast-error ngx-toastr ng-trigger ng-trigger-flyInOut']")
+    private WebElement singInMessage;
 
+
+    protected WebElement find(By locator){
+        return webDriver.findElement(locator);
+    }
+
+    protected void click(By locator){
+        find(locator).click();
+    }
+
+    protected void setText(By locator, String text){
+        find(locator).sendKeys(text);
+    }
 
     public RegisterPage(WebDriver webDriver) {
         this.webDriver = webDriver;
@@ -54,4 +71,44 @@ public class RegisterPage extends BasePage{
        return this.usernameErrorField.getText();
         //return error;
     }
+    public String getEmailFieldRequiredErrorText(){
+
+       return this.emailErrorField.getText();
+        //return error;
+    }
+    public void clearEmailField(){
+
+       this.emailField.sendKeys(Keys.BACK_SPACE);
+        //return error;
+    }
+
+
+    public void fillRegisterForm(String username, String email,String password, String confirmPassword ,String publicInfo){
+        this.usernameField.sendKeys(username);
+        this.passwordField.sendKeys(password);
+        this.confirmPasswordField.sendKeys(confirmPassword);
+        this.emailField.sendKeys(email);
+        this.publicInfoField.sendKeys(publicInfo);
+        this.birthDayField.sendKeys("11111111");
+        this.signInButton.click();
+    }
+    public void fillUsernameField(String username){
+        this.usernameField.sendKeys(username);
+    }
+    public void fillEmailField(String email){
+        this.emailField.sendKeys(email);
+    }
+
+    public void onSignInMessage(String message){
+        WebDriverWait signInMessageWait = new WebDriverWait(this.webDriver, Duration.ofMillis(1500));
+        try {
+            signInMessageWait.until(
+                    ExpectedConditions.textToBePresentInElement(
+                            this.singInMessage
+                            , message));
+        }catch(TimeoutException exception){
+            Assert.fail("Sign in message is not present. Inner exception: " + exception);
+        }
+    }
+
 }
--- src/test/java/RegisterPageTests/RegisterPageTests.java
+++ src/test/java/RegisterPageTests/RegisterPageTests.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import page.factory.RegisterPage;
 
@@ -22,41 +23,64 @@ public class RegisterPageTests  {
     }
     @AfterClass
     public void tearDown(){
-
         webDriver.quit();
     }
-
 
     @Test
     public void testSuccessRegisterPageTransition(){
      // Successful register (popup)
-
     }
+
+    @DataProvider(name="getUsers")
+    public Object[][] getUsers(){
+        return new Object[][]{
+                {"testandy","test@test.com","S3V3+9RPC7F,=Dh", "S3V3+9RPC7F,=Dh", "Public info"}};
+    }
+
     @Test
-    public void userNameTakenMessage(){
-        // Username is Taken
-        //testandy
+    public void userNameEmptyFieldMessage(){
        RegisterPage registerPage = new RegisterPage(webDriver);
        registerPage.isUrlLoaded();
-       Assert.assertEquals(registerPage.usernameFieldRequiredError(), "This field is required!", "Error message is incorrect!");
-
-
+       Assert.assertEquals(registerPage.usernameFieldRequiredError(),
+               "This field is required!", "Error message is incorrect!");
     }
+
+    @Test(dataProvider = "getUsers")
+    public void userNameTakenMessage(String username, String email,String password, String confirmPassword ,String publicInfo){
+        // Username is Taken
+        RegisterPage registerPage = new RegisterPage(webDriver);
+        registerPage.isUrlLoaded();
+        registerPage.fillRegisterForm(username, email, password, confirmPassword , publicInfo);
+        registerPage.onSignInMessage("Username taken");
+    }
+
     @Test
     public void minRequiredSymbolsForUsername(){
      //Minimum 2 characters !
         //sendkeys only 1 symbol
+        RegisterPage registerPage = new RegisterPage(webDriver);
+        registerPage.fillUsernameField("a");
+        Assert.assertEquals(registerPage.usernameFieldRequiredError(),
+                "Minimum 2 characters !", "Error message is incorrect!");
     }
 
     @Test
     public void emailFieldFillRequiredMessage (){
         //This field is required!
         //sendkeys 1 symbol and delete it
+        RegisterPage registerPage = new RegisterPage(webDriver);
+        registerPage.fillEmailField("a");
+        registerPage.clearEmailField();
+
+        Assert.assertEquals(registerPage.getEmailFieldRequiredErrorText(),
+                "This field is required!", "Error message is incorrect!");
     }
+
     @Test
-    public void passwordsDontMatch(){
+    public void passwordsDoNotMatch(){
         //Passwords do not match!
     }
+
     @Test
     public void minimumPasswordChars(){
         //Minimum 6 characters !
@@ -68,6 +92,7 @@ public class RegisterPageTests  {
         //Must contain digit and uppercase letter!
         //type  symbols without Uppercase
     }
+
     @Test
     public void passwordRequired(){
         //This field is required!
